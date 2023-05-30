332b62fc1961eac325b85557397c39a1200e1512
hbturaki <hbturaki@gmail.com>
2023-05-30 10:54:56 +0100

    production update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,19 +11,17 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.default_url_options = { :host => 'photoapp.herokuapp.com', :protocol =>'http'}
- config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'example@gmail.com',
-    password:             'Your gmail password',
-    authentication:       'plain'
+  config.action_mailer.default_url_options = { :host => 'huxaifa-photoapp.herokuapp.com', :protocol =>'http'}
+config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
